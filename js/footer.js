631593af0af850568264bc48506383b5cb82920f
Zero9BSC <franconicolasjones@gmail.com>
2023-05-15 20:49:35 -0300

    Index Update

---
+++ js/footer.js
@@ -0,0 +1,38 @@
+myFooter=`
+            <div class="footer-section">
+                <h4 class="footer-title">Navegación</h4>
+                <ul class="footer-list">
+                    <li class="footer-list-item">
+                        <a class="footer-link" href="/">Inicio</a>
+                    </li>
+                    <li class="footer-list-item">
+                        <a class="footer-link" href="#">Productos</a>
+                    </li>
+                    <li class="footer-list-item">
+                        <a class="footer-link" href="#">Promociones</a>
+                    </li>
+                    <li class="footer-list-item">
+                        <a class="footer-link" href="#">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-section">
+                <h4 class="footer-title">Redes Sociales</h4>
+                <ul class="footer-list">
+                    <li class="footer-list-item">
+                        <a class="footer-link"
+                            href="https://www.facebook.com/profile.php?id=100072489496727">Facebook</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-section">
+                <h4 class="footer-title">Contacto</h4>
+                <h3>2804-976964</h3>
+            </div>
+            <div class="footer-section dev">
+                <h2>Dolce&Ricco <h3>made by Jones Franco</h3></h2>
+                <a href="https://github.com/Zero9BSC">Github Zero9BSC</a>
+            </div>
+`
+
+document.querySelector("footer").innerHTML=myFooter;
